79d79a8bf4473f4d4c9dec2f7caa3f6bbfb3d2fc
Oliver Warke <2324909W@student.gla.ac.uk>
2019-02-26 15:31:13 +0000

    urls

--- nagme/urls.py
+++ nagme/urls.py
@@ -17,7 +17,29 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from nagme import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^welcome/',
+        views.welcome, name ='welcome'),
+    url(r'^login/',
+        views.login,  name= 'login'),
+    url(r'^registration/',
+        views.login, name='registration'),
+    url(r'^userhome/',
+        views.userhome, name='userhome'),
+    url(r'^userhome/account/',
+        views.acount, name='account'),
+    url(r'^userhome/account/account_details/',
+        views.account_details, name='account_details'),
+    url(r'^userhome/account/account_details/account_password',
+        views.account_password, name='account_password'),
+    url(r'^userhome/addnag/',
+        views.addnag, name='addnag'),
+    url(r'^userhome/support/',
+        views.support, name='support'),
+    url(r'^userhome/categories_list/',
+        views.categories, name='categories'),
+    url(r'^userhome/category/',
+        views.category, name='category')
+]
--- nagme/views.py
+++ nagme/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
-# Create your views here.
+def welcome():
+
+def login():
+
+def registration():
+
+def userhome():
+
+def account():
+
+def account_details():
+
+def account_password():
+
+def addnag():
+
+def support():
+
+def categories():
+
+def category():
+
